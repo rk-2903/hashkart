172074b3281d6add66690259e3826576b60cd30a
rahulkumar <rahulkumar6611@gmail.com>
2021-11-06 23:30:22 +0530

    stable version 1

--- hashkart-orderManagement/src/test/java/com/hashkart/orderManagement/HashkartOrderDetailsManagementApplicationTests.java
+++ hashkart-orderManagement/src/test/java/com/hashkart/orderManagement/HashkartOrderDetailsManagementApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class HashkartOrderManagementApplicationTests {
+class HashkartOrderDetailsManagementApplicationTests {
 
 	@Test
 	void contextLoads() {
--- hashkart-userManagement/src/main/java/com/hashkart/userManagement/model/User.java
+++ hashkart-userManagement/src/main/java/com/hashkart/userManagement/model/User.java
@@ -9,16 +9,16 @@ import javax.persistence.Table;
 public class User {
 
     @Id
-    private String id;
+    private String userId;
     private String name;
     private String aadhaar;
 
-    public String getId() {
-        return id;
+    public String getUserId() {
+        return userId;
     }
 
-    public void setId(String id) {
-        this.id = id;
+    public void setUserId(String userId) {
+        this.userId = userId;
     }
 
     public String getName() {
